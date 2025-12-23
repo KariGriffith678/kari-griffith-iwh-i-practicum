1b75a62ed5e0291892a2de71bfe3eab2fae68549
KariGriffith678 <karigriffith678@gmail.com>
2025-12-23 10:03:01 -0600

    Update index.js
    port

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
